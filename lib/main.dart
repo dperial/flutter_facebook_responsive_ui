14711199d816d0f1f90662f4dda0fb4deb88ad66
pblerial <dprial44@gmail.com>
2021-12-18 13:38:27 +0100

    facebookUi page, with more widget. ( status, post, profile and more)

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/screen/screens.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Facebook UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold,
+      ),
+      home: NavScreen(),
+    );
+  }
+}
+
---
+++ lib/widgets/create_post_container.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/models/user_model.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
+
+class CreatePostContainer extends StatelessWidget {
+  final User currentUser;
+
+  const CreatePostContainer({Key key, this.currentUser,}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 0.0),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              ProfileAvatar(imageUrl: currentUser.imageUrl),
+              const SizedBox(width: 8.0,),
+              Expanded(
+                child: const TextField(
+                  decoration: InputDecoration.collapsed(hintText: 'What\' soon your mind'),
+                ),
+              )
+            ],
+          ),
+          const Divider(
+            height: 10.0,
+            thickness: 0.5,
+          ),
+          Container(
+            height: 40.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FlatButton.icon(
+                  onPressed: () => print('Live'),
+                  icon: const Icon(
+                    Icons.videocam,
+                    color: Colors.red,
+                  ),
+                  label: const Text('Live'),
+                ),
+                const VerticalDivider(width: 8.0,),
+                FlatButton.icon(
+                  onPressed: () => print('Photo'),
+                  icon: const Icon(
+                    Icons.photo_library,
+                    color: Colors.green,
+                  ),
+                  label: const Text('Photo'),
+                ),
+                const VerticalDivider(width: 8.0,),
+                FlatButton.icon(
+                  onPressed: () => print('Room'),
+                  icon: const Icon(
+                    Icons.video_call,
+                    color: Colors.purpleAccent,
+                  ),
+                  label: const Text('Room'),
+                ),
+              ],
+            ),
+          )
+        ]
+      ),
+    );
+  }
+}
